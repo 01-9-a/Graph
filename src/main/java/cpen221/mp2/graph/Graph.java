7b00f462d20308700d7362a003689aabc0414eaf
Victor1031 <110273252+Victor1031@users.noreply.github.com>
2022-11-12 22:30:03 -0800

    Update Graph.java

--- src/main/java/cpen221/mp2/graph/Graph.java
+++ src/main/java/cpen221/mp2/graph/Graph.java
@@ -449,14 +449,17 @@ public class Graph<V extends Vertex, E extends Edge<V>> implements ImGraph<V, E>
      */
     @Override
     public boolean addEdge(E e) {
-            if(!edge(e)){
-                if(vertex(e.v1())&&vertex(e.v2())){
-                    al.get(e.v1()).add(e);
-                    al.get(e.v2()).add(e);
-                }
-                return true;
-            }
+        if (!al.containsKey(e.v1())||!al.containsKey(e.v2())) {
             return false;
+        }
+        if(!edge(e)){
+            if(vertex(e.v1())&&vertex(e.v2())){
+                al.get(e.v1()).add(e);
+                al.get(e.v2()).add(e);
+            }
+            return true;
+        }
+        return false;
     }
     /**
      * Check if an edge is part of the graph
